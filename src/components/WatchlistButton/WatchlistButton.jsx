e4850410e14bd6fa3495c3dd34070a01b2ea5f97
Emilia Myrup <myrupemilia@gmail.com>
2025-04-14 16:15:29 +0200

    fixed favlist in watchpage

--- src/components/WatchlistButton/WatchlistButton.jsx
+++ src/components/WatchlistButton/WatchlistButton.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { useFavorites } from '../../context/favoritesContext';
 
-function WatchlistButton({ movie, favorites, setFavorites }) {
+function WatchlistButton({ movie }) {
+  const { favorites, addFavorites, removeFavorites }  = useFavorites()
+
   return (
     <button
       onClick={(event) => {
         event.preventDefault();
+    
       }}
     >
       Lägg till
