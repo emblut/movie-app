a88c07f62ff267e66723795343ef1233dd3ef332
emblut <embla.holgersson@hotmail.com>
2025-04-15 11:46:00 +0200

    Fixed logo bug in header

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.jpg'
 
 function Logo() {
   return (
     <Link to={'/'}>
-      <img className='header__logo' src='./src/assets/logo.jpg' alt='Logo' />
+      <img className='header__logo' src={logo} alt='Logo' />
     </Link>
   );
 }
