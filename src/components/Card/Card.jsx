f44b79313092c3875180085f0dd3df3cafd1bc17
Belinda <belindacanovic@hotmail.com>
2025-04-16 10:01:19 +0200

    fixade missing poster

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,19 @@
 import './Card.css';
 import { Link } from 'react-router-dom';
 import WatchlistButton from '../WatchlistButton/WatchlistButton';
-
-
+import missingPoster from '../../assets/missing-poster.jpg'; 
 
 function Card({ movie }) {
-  console.log(movie);
+  
+  const posterSrc = movie.Poster !== 'N/A' ? movie.Poster : missingPoster;
+  
   return (
     <Link to={`/details/${movie.imdbID}`}>
       <article className='card'>
-        <WatchlistButton
-          movie={movie}
-        />
+        <WatchlistButton movie={movie} />
         <img
           className='card__img'
-          src={movie.Poster}
+          src={posterSrc}
           alt={`${movie.Title} poster`}
         />
         <p className='card__title'>{movie.Title}</p>
